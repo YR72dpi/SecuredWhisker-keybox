4b9daf2520bee0d41a27301242a9063f4ec238bd
Rousselle <ylan.rousselle76@gmail.com>
2026-02-10 22:16:45 +0100

    refactor screenManager.py to clean up comments and improve readability

--- programme/screenManager.py
+++ programme/screenManager.py
@@ -6,23 +6,13 @@ from PIL import Image, ImageDraw, ImageFont
 def printText(epd, text, x, y):
     image = Image.new('1', (epd.height, epd.width), 255)  # 255: blanc, 0: noir
     draw = ImageDraw.Draw(image)
-    
     font15 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 15)
-    # font24 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 24)
-    
+    # font24 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 24)    
     draw.text((x, y), text, font=font15, fill=0)  # fill=0 pour noir
-    
-    # Afficher l'image sur l'écran
     epd.display(epd.getbuffer(image))
-
 
 def clearAndSleep(epd):
     """Applique une image blanche neutre puis éteint l'écran"""
-    # Créer une image blanche
     image = Image.new('1', (epd.height, epd.width), 255)  # 255: blanc
-    
-    # Afficher l'image blanche sur l'écran
-    epd.display(epd.getbuffer(image))
-    
-    # Mettre l'écran en veille
+    epd.display(epd.getbuffer(image))    
     epd.sleep()
