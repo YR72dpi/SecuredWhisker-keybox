515af12a383e484ce09419510d5c0dff94d3fe89
Rousselle <ylan.rousselle76@gmail.com>
2026-02-14 00:52:52 +0100

    Remove unused picture directory path from main.py

--- programme/main.py
+++ programme/main.py
@@ -2,7 +2,6 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
-# picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic/2in13')
 fontdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'font')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
