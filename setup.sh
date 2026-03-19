674f9d38b8b128222eaf09030168f19842b3605a
Rousselle <ylan.rousselle76@gmail.com>
2026-03-19 16:45:56 +0100

    feat(setup): réorganiser le script d'installation et ajouter le service systemd pour SW Keybox

--- setup.sh
+++ setup.sh
@@ -3,11 +3,18 @@ sudo apt-get install git -y
 sudo apt-get install python3-pip -y
 sudo apt-get install python3-pil -y
 sudo apt-get install python3-numpy -y
-sudo pip3 install spidev
-
 sudo apt install python3-gpiozero
-
 sudo apt install python3-smbus
+
+sudo pip3 install spidev
 
 mv ./programme/data.example.json ./programme/data.json
 sudo chmod 700 ./programme/data.json
+
+sudo mv ./sw-keybox.service /etc/systemd/system/sw-keybox.service
+sudo chown root:root /etc/systemd/system/sw-keybox.service
+sudo chmod 755 /etc/systemd/system/sw-keybox.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable sw-keybox.service
+sudo systemctl start sw-keybox.service
